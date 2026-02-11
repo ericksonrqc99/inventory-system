6d5c26aeccfd9e2897f17354bdc0eea2ca242ffa
ericksonrqc99 <erickson.quispe@upeu.edu.pe>
2026-02-11 00:19:22 -0500

    feat: :card_file_box: complete the model country

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
-    //
+    protected $fillable = [
+        'iso2',
+        'phonecode',
+        'capital',
+        'currency',
+        'currency_symbol',
+        'native',
+        'timezones',
+    ];
+
+    public function regions()
+    {
+        return $this->hasMany(Region::class);
+    }
+
+    public function cities()
+    {
+        return $this->hasManyThrough(City::class, Region::class);
+    }
+
+
+    protected $casts = [
+        'timezones' => 'array',
+    ];
 }
