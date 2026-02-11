0560e3e71deb96a103fd3bca9393bf2caa9551f8
ericksonrqc99 <erickson.quispe@upeu.edu.pe>
2026-02-11 01:27:24 -0500

    refactor: :adhesive_bandage: change the name of the column from state_id to region_id in the cities migration.

--- database/migrations/2026_02_11_042359_create_cities_table.php
+++ database/migrations/2026_02_11_042359_create_cities_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('cities', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('state_id')->constrained()->onDelete('restrict');
+            $table->foreignId('region_id')->constrained()->onDelete('restrict');
             $table->string('native', 70);
             $table->string('type', 30);
             $table->timestamps();
