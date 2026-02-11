0241a16c79548c3fffbfb815110e76d74ed83640
ericksonrqc99 <erickson.quispe@upeu.edu.pe>
2026-02-11 00:30:48 -0500

    feat: :card_file_box: complete the model region

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Region extends Model
 {
-    //
+    protected $fillable = [
+        'country_id',
+        'fips_code',
+        'type',
+        'native',
+    ];
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function cities()
+    {
+        return $this->hasMany(City::class);
+    }
 }
