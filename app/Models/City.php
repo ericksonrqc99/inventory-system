b3e58badaf002564bec85beb211b00fcf4e4678d
ericksonrqc99 <erickson.quispe@upeu.edu.pe>
2026-02-11 01:37:42 -0500

    feat: :card_file_box: complete the CountrySeeder

--- app/Models/City.php
+++ app/Models/City.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     protected $fillable = [
-        'state_id',
+        'region_id',
         'native',
         'type',
     ];
--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Country;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,23 @@ class CountrySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $csvFile = fopen(base_path('data/world-database/peru/countries.csv'), 'r');
+        $firstline = true;
+        while (($data = fgetcsv($csvFile, 2000, ',')) !== false) {
+            if (! $firstline) {
+                Country::create([
+                    'id' => $data['0'],
+                    'iso2' => $data['3'],
+                    'phonecode' => $data['5'],
+                    'capital' => $data['6'],
+                    'currency' => $data['7'],
+                    'currency_symbol' => $data['9'],
+                    'native' => $data['11'],
+                    'timezones' => $data['22'],
+                ]);
+            }
+            $firstline = false;
+        }
+        fclose($csvFile);
     }
 }
