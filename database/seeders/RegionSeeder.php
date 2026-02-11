cd0945ea525f3bdde4a913e8ca761932571aa1ac
ericksonrqc99 <erickson.quispe@upeu.edu.pe>
2026-02-11 01:53:04 -0500

    feat: :card_file_box: complete the RegionSeeder

--- database/seeders/RegionSeeder.php
+++ database/seeders/RegionSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Region;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,20 @@ class RegionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+         $csvFile = fopen(base_path('data/world-database/peru/states.csv'), 'r');
+        $firstline = true;
+        while (($data = fgetcsv($csvFile, 2000, ',')) !== false) {
+            if (! $firstline) {
+                Region::create([
+                    'id' => $data['0'],
+                    'country_id' => $data['2'],
+                    'name' => $data['1'],
+                    'fips_code' => $data['7'],
+                    'type' => $data['8'],
+                ]);
+            }
+            $firstline = false;
+        }
+        fclose($csvFile);
     }
 }
