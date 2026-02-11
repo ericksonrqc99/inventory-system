c14ff98edfb9c7469539cc271950b2a28e345025
ericksonrqc99 <erickson.quispe@upeu.edu.pe>
2026-02-11 00:41:28 -0500

    feat: :card_file_box: complete the model city

--- app/Models/City.php
+++ app/Models/City.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
-    //
+    protected $fillable = [
+        'state_id',
+        'native',
+        'type',
+    ];
+
+    public function region()
+    {
+        return $this->belongsTo(Region::class);
+    }
 }
